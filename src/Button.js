9edb14e60e1e21004cd82d6ca2f3a59ffc21e2fb
HyangRim <curomame@naver.com>
2021-11-16 21:53:50 +0900

    왜 여기서 새로고침이 자꾸 되는지 늪에 빠져버렸다

--- src/Button.js
+++ src/Button.js
@@ -3,7 +3,7 @@ import styles from "./Button.module.css"
 
 
 function Button({text}){
-  return <button classNmae={styles.btn}>{text}</button>;
+  return <button className={styles.btn}>{text}</button>;
 }
 
 Button.propTypes = {
