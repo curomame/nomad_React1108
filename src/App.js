df3e5c1bf5cde80c496ba5c2b328c932e3b448b4
HyangRim <curomame@naver.com>
2021-11-17 23:52:14 +0900

    Before React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from "react";
+import Movie from "./Movie";
 
 function App() {
 
@@ -23,18 +24,19 @@ console.log(movies);
 
 return (
 <div>
-  {loading ? <h1>Lodaing....</h1> : <div>{movies.map((movie) => 
-  <div key={movie.id}> 
-  <img src={movie.medium_cover_image}/>
-  <h2>{movie.title}</h2>
-  <p>{movie.summary}</p>
-  <ul>
-    <li>
-      {movie.genres.map(g => <li key={g}>{g}</li>)}
-    </li>
-  </ul>
-  
-  </div>)}</div>}
+  {loading ? <h1>Lodaing....</h1> : (
+  <div>
+    {movies.map((movie) => (
+    <Movie 
+    key={movie.id}
+    coverImg={movie.medium_cover_image}
+    title={movie.title}
+    summary={movie.summary}
+    genres={movie.genres}
+    />
+  ))}
+    
+  </div>)}
 </div>
 );
 
