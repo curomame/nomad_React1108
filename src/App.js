0407d8615edeba3fbe9215fcd65190307d775aa4
HyangRim <curomame@naver.com>
2021-11-18 23:33:55 +0900

    route not switch

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
-import {useState, useEffect} from "react";
-import Movie from "./Movie";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Home from "./routes/Home"
+import Details from "./routes/Details"
 
 function App() {
-
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await ( 
-      await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-useEffect(() => {
-  getMovies();
-}, []);
-
-console.log(movies);
-
 return (
-<div>
-  {loading ? <h1>Lodaing....</h1> : (
-  <div>
-    {movies.map((movie) => (
-    <Movie 
-    key={movie.id}
-    coverImg={movie.medium_cover_image}
-    title={movie.title}
-    summary={movie.summary}
-    genres={movie.genres}
-    />
-  ))}
-    
-  </div>)}
-</div>
+<Router>
+  <Routes>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="/movie" element={<Details />}></Route>
+  </Routes>
+</Router>
 );
-
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,16 @@
+import {Link} from "react-router-dom"
+
 import PropTypes from "prop-types";
+
+
 
 function Movie({coverImg, title, summary, genres}) {
   return (
   <div> 
   <img src={coverImg} alt="{title}"/>
-  <h2>{title}</h2>
+  <h2>
+    <Link to="/movie">{title}</Link>
+    </h2>
   <p>{summary}</p>
   <ul>
     <li>
